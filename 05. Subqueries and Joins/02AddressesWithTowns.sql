9b0944b1192d9e03735a06457bbc4ca8c0c1f548
IvanAbadzhiev01 <ivan087611@gmail.com>
2024-09-22 22:39:57 +0300

    Add new task

---
+++ 05. Subqueries and Joins/02AddressesWithTowns.sql
@@ -0,0 +1,12 @@
+SELECT TOP 50
+FirstName,
+LastName,
+t.[Name],
+a.AddressText
+FROM Employees AS e
+JOIN Addresses AS a ON e.AddressID = a.AddressID
+JOIN Towns AS t  ON a.TownID = t.TownID
+ORDER BY  FirstName, LastName
+
+
+
