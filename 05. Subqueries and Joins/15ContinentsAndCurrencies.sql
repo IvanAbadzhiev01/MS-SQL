dc57871c6705b5dc7b5cf9d275ff729af7fba035
IvanAbadzhiev01 <ivan087611@gmail.com>
2024-09-25 23:18:12 +0300

    Add new task

---
+++ 05. Subqueries and Joins/15ContinentsAndCurrencies.sql
@@ -0,0 +1,16 @@
+SELECT 
+ContinentCode,
+CurrencyCode,
+CurrencyUsage
+FROM
+(SELECT
+*,
+DENSE_RANK() OVER (PARTITION BY ContinentCode ORDER BY CurrencyUsage DESC ) AS CurrencyRank
+FROM 
+(
+SELECT ContinentCode, CurrencyCode, COUNT(CurrencyCode) AS CurrencyUsage
+FROM Countries
+GROUP BY ContinentCode,CurrencyCode 
+) AS CoreQuery
+WHERE CurrencyUsage > 1 ) AS SecondQuery
+WHERE CurrencyRank = 1
